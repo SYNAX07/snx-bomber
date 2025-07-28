eea843216fcf587c6ebf7d388a81aea73b157f88
Ashwani <162049637+SYNAX07@users.noreply.github.com>
2025-07-28 11:35:33 +0530

    Update bomb.js

--- pages/api/bomb.js
+++ pages/api/bomb.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
     if (jsonData.mail.multi) apis.push(...jsonData.mail.multi);
   }
 
-  const fetch = (await import('node-fetch')).default;
+  import fetch from 'node-fetch';
   const results = [];
 
   for (let i = 0; i < count; i++) {
